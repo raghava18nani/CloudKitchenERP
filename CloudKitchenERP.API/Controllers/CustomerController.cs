32520ffee14c90fa59701b24ed267c6e6c5852c4
TungalaBhavani2352 <bhavanitungala64@gmail.com>
2026-07-05 00:21:00 +0530

    feat(customer): complete customer profile management module

--- CloudKitchenERP.API/Controllers/CustomerController.cs
+++ CloudKitchenERP.API/Controllers/CustomerController.cs
@@ -1,20 +1,70 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CloudKitchenERP.Application.Interfaces;
+using CloudKitchenERP.Contracts.Customer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CloudKitchenERP.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 public class CustomerController : ControllerBase
 {
-    [Authorize]
-    [HttpGet("profile")]
-    public IActionResult GetProfile()
+    private readonly ICustomerService _customerService;
+
+    public CustomerController(ICustomerService customerService)
     {
-        return Ok(new
-        {
-            Message = "Welcome to Cloud Kitchen ERP!",
-            Time = DateTime.Now
-        });
+        _customerService = customerService;
+    }
+
+    [HttpGet("profile")]
+    public async Task<IActionResult> GetProfile()
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim == null)
+            return Unauthorized("User ID claim not found.");
+
+        int userId = int.Parse(userIdClaim.Value);
+
+        var customer = await _customerService.GetProfileAsync(userId);
+
+        if (customer == null)
+            return NotFound();
+
+        return Ok(customer);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateCustomerRequest request)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim == null)
+            return Unauthorized("User ID claim not found.");
+
+        int userId = int.Parse(userIdClaim.Value);
+        var customer = await _customerService.CreateAsync(userId, request);
+
+        return Ok(customer);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(UpdateCustomerRequest request)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim == null)
+            return Unauthorized("User ID claim not found.");
+
+        int userId = int.Parse(userIdClaim.Value);
+
+        var success = await _customerService.UpdateAsync(userId, request);
+
+        if (!success)
+            return NotFound();
+
+        return NoContent();
     }
 }
